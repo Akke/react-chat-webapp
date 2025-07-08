ab6b021c07fd95e523ed529cf04400230335ec52
Axel Lindgren Wenell <atomwar.box@gmail.com>
2025-07-08 22:23:59 +0200

    initial commit

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,33 @@
+import "./Login.css";
+
+const Login = () => {
+    return (
+        <div className="login-wrapper">
+            <div className="login-container">
+                <div className="login-banner"></div>
+                <div className="login-right-container">
+                    <h1>Login to Chatify</h1>
+                    <p>Stay connected across any distance — with the people who matter most.</p>
+
+                    <form className="login-form">
+                        <input type="text" name="username" placeholder="Username" />
+                        <input type="password" name="password" placeholder="Password" />
+                        <button type="submit">Login</button>
+                    </form>
+
+                    <div className="registration-notice">
+                        <div className="text">Don't have an account?</div>
+                    </div>
+
+                    <div className="registration-button-container">
+                        <a href="#">
+                            <button className="secondary">Create an Account</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
