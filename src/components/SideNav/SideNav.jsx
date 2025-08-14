8f3bea771605ee9d3fc013f9b061ce4f2b93e09d
Akke <sirpurpleness@gmail.com>
2025-08-14 21:49:06 +0200

    added togglable sidenav

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,21 +1,32 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../contexts/AuthProvider";
 import "./SideNav.css";
 import { Link } from "react-router-dom";
-import { IoLogOut, IoPersonCircle } from "react-icons/io5";
+import { IoChevronBack, IoChevronForward, IoLogOut, IoPersonCircle } from "react-icons/io5";
 
 const SideNav = () => {
     const { clearAuth } = useContext(AuthContext);
+    const [isToggled, setIsToggled] = useState(true);
 
     return (
-        <nav className="side-nav">
+        <nav className={`side-nav ${!isToggled ? "minimized" : ""}`}>
+            <div className="side-header">
+                <div className="toggle-button" onClick={() => setIsToggled(!isToggled)}>
+                    {isToggled ?
+                        <IoChevronBack />
+                    :   <IoChevronForward />
+                    }
+                </div>
+                <span className="logo">Chatify</span>
+            </div>
+
             <ul>
                 <Link to="/profile">
-                    <li><IoPersonCircle /> Profile</li>
+                    <li><IoPersonCircle /> <span>Profile</span></li>
                 </Link>
 
                 <Link to="/" onClick={clearAuth} className="logout">
-                    <li><IoLogOut /> Logout</li>
+                    <li><IoLogOut /> <span>Logout</span></li>
                 </Link>
             </ul>
         </nav>
